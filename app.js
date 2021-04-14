82143b1e1383015650a85724a7cec9200ae351fd
Patrick Groves <patrickalexgroves@gmail.com>
2021-04-14 12:50:40 -0600

    fully functioning api calls based on input

--- app.js
+++ app.js
@@ -2,47 +2,46 @@ checks = document.getElementsByClassName("checks")
 submitBtn = document.querySelector("#submit")
 console.log(checks)
 
+const CALLPATH = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc"
 function randLLQuery(){
     const LATF = 6.15649818383
     const LONGF = 7.35564545789
     const lat = (30.22443 + (Math.random()/LATF)).toFixed(5)
     const long = (-97.82211 + (Math.random()/LONGF)).toFixed(5)
-    return `${lat},${long}`
+    return `&location=${lat},${long}`
 }
-console.log(randLLQuery())
 
-checks[0].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
-checks[1].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
-checks[2].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
-checks[3].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
-checks[4].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
-checks[5].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
-checks[6].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
-checks[7].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
-checks[8].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
-checks[9].innerHTML = `?${randLLQuery()}&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc`
+checks[0].name = CALLPATH + randLLQuery() + "&radius=5000&type=cafe"
+checks[1].name = CALLPATH + randLLQuery() + "&radius=5000&type=park"
+checks[2].name = CALLPATH + randLLQuery() + "&radius=5000&type=tourist_attraction"
+checks[3].name = CALLPATH + randLLQuery() + "&radius=5000&type=restaurant&keyword=lunch"
+checks[4].name = CALLPATH + randLLQuery() + "&radius=5000&type=tourist_attraction"
+checks[5].name = CALLPATH + randLLQuery() + "&radius=5000&type=museum"
+checks[6].name = CALLPATH + randLLQuery() + "&radius=5000&type=restaurant&keyword=dinner"
+checks[7].name = CALLPATH + randLLQuery() + "&radius=5000&type=tourist_attraction"
+checks[8].name = CALLPATH + randLLQuery() + "&radius=5000&type=bar&keyword=music"
+checks[9].name = CALLPATH + randLLQuery() + "&radius=5000&type=night_club"
 
 
-//create func to return random lat/long within atx geo-rectangle DONE
 //feed params to showApi: user input translates to type and keyword queries
-//      rand lat/long passed as location value
-const showApi = async () => {
-    //place search for type restaurant with keyword dinner in Austin
-    //within 600m of lat/long 30.2672,-97.741
-    const data = await fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=30.2672,-97.741&radius=600&opennow&type=restaurant&keyword=dinner&key=AIzaSyD4iATDK-gfM5Q_mSWMhrfBjxnval2wzvc")
+const getPlace= async (query) => {
+    const data = await fetch(query)
     const json = await data.json()
-    return json
+    return json.results[0]
 
 }
+
 
 submitBtn.addEventListener("click", async () =>{
-    // json = await showApi()
-    // console.log(json)
-    // for(place of json.results){
-    //     console.log(place.name)
-    // }
+    checkList = []
     for(check of checks){
-        console.log(check.checked)
+        if(check.checked){
+            checkList.push(check.name)
+        }
     }
+
+    const placePromises = checkList.map(async (url) => await getPlace(url))
+    const placeResults = await Promise.all(placePromises) 
+    console.log(placeResults)
 
 })
